541e359a7aac40a2ad2d288ea98a020bfd4a3c2b
유혜승 <yuhyeseung@ddunii-macbook.local>
2023-05-10 12:19:17 +0900

    230510 coding_test (SQL, Algorism)

--- CodingTest/LeetCode.sql
+++ CodingTest/LeetCode.sql
@@ -836,3 +836,57 @@ FROM temp_01 a
     JOIN department b
     ON a.departmentId = b.id
 WHERE rnk = 1;
+
+-------------------------
+-- 262. Trips and Users
+
+with temp_01 as
+(
+    SELECT *, 
+        case when status LIKE "cancelled%" then 1 else 0 end as is_cancelled
+    FROM trips
+    WHERE client_id not in (SELECT users_id FROM users WHERE banned="Yes")
+        AND driver_id not in (SELECT users_id FROM users WHERE banned="Yes")
+    )
+
+SELECT request_at as Day, 
+    ROUND(sum(is_cancelled) / count(id), 2) as `Cancellation Rate`
+FROM temp_01
+WHERE request_at between str_to_date('2013-10-01', '%Y-%m-%d') and str_to_date('2013-10-03', '%Y-%m-%d')
+GROUP BY request_at;
+    
+-- 1179. Reformat Department Table
+
+with temp_01 as
+(
+    SELECT id, 
+        case when month = "Jan" then revenue end as Jan_Revenue,
+        case when month = "Feb" then revenue end as Feb_Revenue,
+        case when month = "Mar" then revenue end as Mar_Revenue,
+        case when month = "Apr" then revenue end as Apr_Revenue,
+        case when month = "May" then revenue end as May_Revenue,
+        case when month = "Jun" then revenue end as Jun_Revenue,
+        case when month = "Jul" then revenue end as Jul_Revenue,
+        case when month = "Aug" then revenue end as Aug_Revenue,
+        case when month = "Sep" then revenue end as Sep_Revenue,
+        case when month = "Oct" then revenue end as Oct_Revenue,
+        case when month = "Nov" then revenue end as Nov_Revenue,
+        case when month = "Dec" then revenue end as Dec_Revenue
+    FROM department
+    ORDER BY 1
+    )
+SELECT id, 
+    sum(Jan_Revenue) as Jan_Revenue, 
+    sum(Feb_Revenue) as Feb_Revenue,
+    sum(Mar_Revenue) as Mar_Revenue,
+    sum(Apr_Revenue) as Apr_Revenue,
+    sum(May_Revenue) as May_Revenue,
+    sum(Jun_Revenue) as Jun_Revenue,
+    sum(Jul_Revenue) as Jul_Revenue,
+    sum(Aug_Revenue) as Aug_Revenue,
+    sum(Sep_Revenue) as Sep_Revenue,
+    sum(Oct_Revenue) as Oct_Revenue,
+    sum(Nov_Revenue) as Nov_Revenue,
+    sum(Dec_Revenue) as Dec_Revenue
+FROM temp_01
+GROUP BY id;
