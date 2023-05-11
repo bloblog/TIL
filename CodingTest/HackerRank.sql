786e0eda4044986455ecaa1bc7e15269ece63b26
유혜승 <yuhyeseung@ddunii-macbook.local>
2023-05-11 10:31:36 +0900

    add HackerRank file

---
+++ CodingTest/HackerRank.sql
@@ -0,0 +1,15 @@
+
+[HackerRank 코딩테스트 링크](https://www.hackerrank.com/domains/sql?filters[status][]=unsolved&filters[status][]=solved)
+
+-- Weather Observation Station 5
+
+SELECT min(city), length(city)
+FROM station
+WHERE length(city) = (SELECT MAX(length(city)) FROM station)
+GROUP BY length(city)
+UNION 
+SELECT min(city), length(city)
+FROM station
+WHERE length(city) = (SELECT MIN(length(city)) FROM station)
+GROUP BY length(city)
+ORDER BY 1;
