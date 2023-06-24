cd71880fcc1928fd6e423b6fa97420f676b25c6e
유혜승 <yuhyeseung@ddunii-macbook.local>
2023-06-24 23:59:01 +0900

    230624 coding test (SQL)

--- CodingTest/InterviewBit.sql
+++ CodingTest/InterviewBit.sql
@@ -36,3 +36,84 @@ SELECT GROUP_CONCAT(Name) as Names
 FROM Students
 GROUP BY BirthDate
 ORDER BY BirthDate;
+
+-- Minimum GPA
+
+SELECT CONCAT(DepartmentName, "," ,Name, ",", GPA) as A
+FROM (SELECT DepartmentId, MIN(GPA) as min_gpa
+    FROM Students
+    GROUP BY 1) a
+    LEFT JOIN Students b 
+    ON a.DepartmentId = b.DepartmentId AND a.min_gpa = b.GPA
+    LEFT JOIN Departments c
+    ON b.DepartmentId = c.DepartmentId
+ORDER BY DepartmentName;
+    
+-- Number of offers
+
+SELECT IFNULL(cnt, 0) as Job_Offers
+FROM Students a
+    LEFT JOIN (SELECT Id, count(*) as cnt
+                FROM Jobs
+                WHERE MONTH(Date) = 11
+                GROUP BY Id) b 
+    ON a.Id = b.Id 
+ORDER BY a.Id;
+
+-- Movie Character
+
+SELECT CONCAT(director_first_name, director_last_name) as director_name, movie_title
+FROM movies a
+    JOIN movies_directors b
+    ON a.movie_id = b.movie_id
+    JOIN directors c 
+    ON b.director_id = c.director_id
+WHERE a.movie_id IN (SELECT movie_id
+                    FROM movies_cast
+                    WHERE role = "SeanMaguire");
+                        
+-- Short Films
+
+SELECT movie_title, movie_year
+        , concat(director_first_name, director_last_name) as director_name
+    , concat(actor_first_name, actor_last_name) as actor_name, role 
+FROM movies a
+    JOIN movies_directors b
+    ON a.movie_id = b.movie_id
+    JOIN directors c
+    ON b.director_id = c.director_id
+    JOIN movies_cast d
+    ON a.movie_id = d.movie_id
+    JOIN actors e
+    ON d.actor_id = e.actor_id   
+WHERE movie_time = (SELECT MIN(movie_time)
+                    FROM movies);
+    
+-- Study Deletion
+
+DELETE FROM STUDY WHERE Age between 19 and 22;
+SELECT *
+FROM STUDY;
+    
+-- Actors and their Movies
+
+SELECT movie_title
+FROM movies a
+    JOIN movies_cast b
+    ON a.movie_id = b.movie_id
+WHERE actor_id IN (SELECT actor_id
+                    FROM movies_cast
+                    GROUP BY actor_id
+                    HAVING count(movie_id) >= 2);
+
+-- Tournament 2.0
+
+SELECT ROUND(sum(Result)/count(Result), 4) as `Percentage Wins`
+FROM Matches a
+    LEFT JOIN Team1 b
+    ON a.PlayerId1 = b.Id 
+    LEFT JOIN Team2 c
+    ON a.PlayerId2 = c.Id
+WHERE c.Cheater = 0
+GROUP BY Date
+ORDER BY Date;
